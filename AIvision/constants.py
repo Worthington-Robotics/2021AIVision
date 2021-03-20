860cc1ebc2bdc2861904b412394572b7af963e24
coalman321 <coalman321@users.noreply.github.com>
2021-03-20 15:36:10 -0400

    jetson things

---
+++ AIvision/constants.py
@@ -0,0 +1,11 @@
+class Constants:
+
+    HOST = '10.41.45.2'
+    PORT = 5800
+
+    THETA = 13
+
+    FOV_V = 44.15
+    FOV_H = 51.06
+
+    HEIGHT = .54
---
+++ AIvision/sendBboxData.py
@@ -0,0 +1,58 @@
+import jetson.inference
+import jetson.utils
+import math
+
+from SocketTables.python.socketTableClient import SocketTableClient
+from constants import Constants
+
+net = jetson.inference.detectNet(argv=['--model=torch_200_powercell.onnx', '--input_blob=input_0', '--labels=labels.txt', '--output-cvg=scores', '--output-bbox=boxes'], threshold=0.5)
+camera = jetson.utils.videoSource("/dev/video0")
+display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
+
+def detectBalls():
+    img = camera.Capture()
+    detections = net.Detect(img)
+
+    ball_info = ''
+
+    # print("This is the center data: {}".format(centerXY))
+    # print("This is the width data: {}".format(width))
+    # print("This is the height data: {}".format(height))
+
+    if not not detections:
+        for detection in detections:
+            centerXY = detection.Center
+
+            (x, y) = centerXY
+
+            # x_formated = Constants.HEIGHT * math.tan(90 - ((360 - y) / 720) * Constants.FOV_V + Constants.THETA)
+            # y_formated = x_formated * math.tan(Constants.FOV_H * ((x - 640) / 1280))
+
+            theta_ball = (((y - 360) * (Constants.FOV_V / 720)) + Constants.THETA) * math.pi / 180
+            x_formated = (Constants.HEIGHT - .083)/math.tan(theta_ball)
+            theta_h = (640 - x) * (Constants.FOV_H / 1280) * math.pi / 180
+            y_formated = (x_formated * theta_h)
+
+            print(str(x) + "," + str(y))
+            ball_info += '{}x{}y'.format(x_formated, y_formated)
+
+    return ball_info
+
+def sendData(client, value):
+    client.update('balls', value)
+
+def main():
+    
+
+    client = SocketTableClient(Constants.HOST, Constants.PORT)
+
+    while True:
+        sendData(client, detectBalls())
+        print(detectBalls())
+            
+
+
+if __name__ == '__main__':
+    main()
+    
+ 
